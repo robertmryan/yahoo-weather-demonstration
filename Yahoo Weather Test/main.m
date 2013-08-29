338acfb6a2a4218a69c726408825b21d32fa45d8
robertmryan <robert.ryan@mindspring.com>
2013-08-28 21:53:21 -0400

    Initial Commit

---
+++ Yahoo Weather Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Yahoo Weather Test
+//
+//  Created by Robert Ryan on 8/28/13.
+//  Copyright (c) 2013 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
